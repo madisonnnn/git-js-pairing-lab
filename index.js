0f66c3a4784d25daa47686db77fcd6cf45334db4
Mei Zhu <maggiez0613@gmail.com>
2024-03-28 13:12:52 -0400

    question2 done

--- index.js
+++ index.js
@@ -2,10 +2,21 @@
 
 //Question 1 - Madison
 
-const fiveToOneHundred = () => {
-    for(i = 5; i <= 100; i++){
-        console.log(i)
+// const fiveToOneHundred = () => {
+//     for(i = 5; i <= 100; i++){
+//         console.log(i)
+//     }
+// }
+
+// fiveToOneHundred()
+
+// Question 2 - Mei 
+const multiplesOfThree = () => {
+    for (i = 1; i <= 100; i++) {
+        if (i % 3 === 0) {
+            console.log(i);
+        }
     }
 }
 
-fiveToOneHundred()
+multiplesOfThree();
