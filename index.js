7e3f2a9b13779af4bf6bba7ee00b0b6260c41530
Mei Zhu <maggiez0613@gmail.com>
2024-03-28 13:59:23 -0400

    question4

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
-//Code your solutions in this file
+// //Code your solutions in this file
 
-//Question 1 - Madison
+// //Question 1 - Madison
 
-const fiveToOneHundred = () => {
-    for(i = 5; i <= 100; i++){
-        console.log(i)
+// const fiveToOneHundred = () => {
+//     for(i = 5; i <= 100; i++){
+//         console.log(i)
+//     }
+// }
+
+// fiveToOneHundred()
+
+// // Question 2 - Mei 
+// const multiplesOfThree = () => {
+//     for (i = 1; i <= 100; i++) {
+//         if (i % 3 === 0) {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// multiplesOfThree();
+
+// //Question 3 - Madison
+
+// const multiplesOfThreeOrFive = () => {
+//     for(i = 3; i <= 100; i++){
+//         if(i % 3 === 0 || i % 5 === 0){
+//             console.log(i)
+//         }
+//     }
+// }
+
+// multiplesOfThreeOrFive()
+
+// Question 4 - Mei 
+const untilNum = (numbers) => {
+    for(i = 1; i <= numbers; i++) {
+        console.log(i); 
     }
 }
 
-fiveToOneHundred()
-
-// Question 2 - Mei 
-const multiplesOfThree = () => {
-    for (i = 1; i <= 100; i++) {
-        if (i % 3 === 0) {
-            console.log(i);
-        }
-    }
-}
-
-multiplesOfThree();
-
-//Question 3 - Madison
-
-const multiplesOfThreeOrFive = () => {
-    for(i = 3; i <= 100; i++){
-        if(i % 3 === 0 || i % 5 === 0){
-            console.log(i)
-        }
-    }
-}
-
-multiplesOfThreeOrFive()
+untilNum(5);
