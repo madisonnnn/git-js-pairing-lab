6f061c171d9c7de8eb0385c5a18a2b448fa28eb5
Marcy <madisontolentino@gmail.com>
2024-03-28 14:37:59 -0400

    Final Product

--- index.js
+++ index.js
@@ -1,87 +1,87 @@
 // //Code your solutions in this file
 
-// //Question 1 - Madison
+//Question 1 - Madison
 
-// const fiveToOneHundred = () => {
-//     for(i = 5; i <= 100; i++){
-//         console.log(i)
-//     }
-// }
+const fiveToOneHundred = () => {
+    for(i = 5; i <= 100; i++){
+        console.log(i)
+    }
+}
 
-// fiveToOneHundred()
+fiveToOneHundred()
 
-// // Question 2 - Mei 
-// const multiplesOfThree = () => {
-//     for (i = 1; i <= 100; i++) {
-//         if (i % 3 === 0) {
-//             console.log(i);
-//         }
-//     }
-// }
+// Question 2 - Mei 
+const multiplesOfThree = () => {
+    for (i = 1; i <= 100; i++) {
+        if (i % 3 === 0) {
+            console.log(i);
+        }
+    }
+}
 
-// multiplesOfThree();
+multiplesOfThree();
 
-// //Question 3 - Madison
+//Question 3 - Madison
 
-// const multiplesOfThreeOrFive = () => {
-//     for(i = 3; i <= 100; i++){
-//         if(i % 3 === 0 || i % 5 === 0){
-//             console.log(i)
-//         }
-//     }
-// }
+const multiplesOfThreeOrFive = () => {
+    for(i = 3; i <= 100; i++){
+        if(i % 3 === 0 || i % 5 === 0){
+            console.log(i)
+        }
+    }
+}
 
-// multiplesOfThreeOrFive()
+multiplesOfThreeOrFive()
 
-// Question 4 - Mei 
-// const untilNum = (numbers) => {
-//     for(i = 1; i <= numbers; i++) {
-//         console.log(i); 
-//     }
-// }
+//Question 4 - Mei 
+const untilNum = (numbers) => {
+    for(i = 1; i <= numbers; i++) {
+        console.log(i); 
+    }
+}
 
-// untilNum(5);
+untilNum(5);
 
-// //Question 5 - Madison
+//Question 5 - Madison
 
-// const multiply = (num1,num2) => {
-//     console.log(num1*num2)
-// }
+const multiply = (num1,num2) => {
+    console.log(num1*num2)
+}
 
-// multiply(2,4)
+multiply(2,4)
 
-// // Question 6 - Mei 
-// const add = (num3, num4) => {
-//     if (num3 === num4) {
-//         return ((num3 + num4) * 3);
-//     } else {
-//         return num3 + num4;
-//     }
-// }
+// Question 6 - Mei 
+const add = (num3, num4) => {
+    if (num3 === num4) {
+        return ((num3 + num4) * 3);
+    } else {
+        return num3 + num4;
+    }
+}
 
-// console.log(add(6,6))
+console.log(add(6,6))
 
-// //Question 7 - Madison
+//Question 7 - Madison
 
-// const isNegative = (num) => {
-//     num < 0 ? console.log(true):console.log(false)
-// }
+const isNegative = (num) => {
+    num < 0 ? console.log(true):console.log(false)
+}
 
-// isNegative(6)
+isNegative(6)
 
-// Question 8 - Mei 
-// const triangleArea = (height, base) => {
-//     return ((height * base) * 1/2);
-// }
-// console.log(triangleArea(6,8))
+//Question 8 - Mei 
+const triangleArea = (height, base) => {
+    return ((height * base) * 1/2);
+}
+console.log(triangleArea(6,8))
 
-// //Question 9 - Madison
+//Question 9 - Madison
 
-// const betweenTwentyAndForty = (num) => {
-//     num > 20 && num < 40 ? console.log(true): console.log(false)
-// }
+const betweenTwentyAndForty = (num) => {
+    num > 20 && num < 40 ? console.log(true): console.log(false)
+}
 
-// betweenTwentyAndForty(90)
+betweenTwentyAndForty(90)
 
 // Question 10 - Mei 
 const largest = (num5, num6, num7) => {
